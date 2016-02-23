1ab84bed88162df07bee63b0731d277433ef2f63
Brad Searle <bradley.c.searle@accenture.com>
2016-02-23 10:03:18 -0600

    Display a JS list in the created element. Pass in as a property to the item, then display it with a map function to create the markup around it.

--- app/components/GroceryItemList.jsx
+++ app/components/GroceryItemList.jsx
@@ -5,6 +5,18 @@ module.exports = React.createClass({
     return (
       <div>
         <h1>Grocery Listify</h1>
+        <div>
+          {
+            this.props.items.map(function(item, index) {
+              return (
+                <div>
+                <input type="checkbox" checked={item.purchased} />
+                {item.name}
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
     );
   }
--- app/main.jsx
+++ app/main.jsx
@@ -2,4 +2,16 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var GroceryItemList = require('./components/GroceryItemList.jsx');
 
-ReactDOM.render(<GroceryItemList />, app);
+
+var initial = [{
+  name: "ice cream"
+}, {
+  name: "waffles"
+},{
+  name: "candy",
+  purchased: true
+},{
+  name: "pepsi"
+}];
+
+ReactDOM.render(<GroceryItemList items={initial} />, app);
