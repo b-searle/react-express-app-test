b75f5d7e08c09ea79fe6a859a5944ae7cc3a474a
Brad Searle <bradley.c.searle@accenture.com>
2016-02-23 09:21:36 -0600

    Configure browserify, reactify. Compile main.jsx to app.js using gulp bundle. Update server JS to load linked files from .tmp directory

--- server/main.js
+++ server/main.js
@@ -4,6 +4,6 @@ var app = new express();
 app.get('/', function(req, res) {
   res.render('./../app/index.ejs',{});
 })
-.use(express.static(__dirname + "/../app"));
+.use(express.static(__dirname + "/../.tmp"));
 
 app.listen(7777);
